11d1ed01100cfc7d9186c68d3d391514bfff4e41
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2016-03-19 01:23:27 +0200

    Ch 2

---
+++ source/app.js
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var reactElement = React.createElement('h1');
+ReactDOM.render(reactElement, document.getElementById('react-application'));
